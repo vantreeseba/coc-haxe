cab784c3dc751a9aa57e02fe196f911f39055796
Benjamin Van Treese <vantreeseba@gmail.com>
2020-08-14 16:21:22 -0500

    FIX: Fix incorrect comment in command list.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,6 @@
 import GotoHxml from './GotoHxml';
-// import ChangeHxml from './ChangeHxml';
+import ChangeHxml from './ChangeHxml';
 import RestartClient from './RestartClient';
 import PrintConfig from './PrintConfig';
 
-export default [GotoHxml, RestartClient, PrintConfig, ChangeHxml];
+export default [GotoHxml, RestartClient, PrintConfig];
